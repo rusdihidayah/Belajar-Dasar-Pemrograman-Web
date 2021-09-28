c4dea315abb3db93fe633eac5bb37822305e6b3a
Rusdi Hidayah <48021916+rusdihidayah@users.noreply.github.com>
2021-09-29 02:15:47 +0700

    Add files via upload

---
+++ KopiIndonesia/assets/script.js
@@ -0,0 +1,10 @@
+alert('Selamat Datang Pecinta Kopi!');
+
+const judul = document.getElementById('judul');
+judul.innerHTML = 'Kopi Indonesia'
+
+const article = document.querySelector('article#etimologi');
+
+
+const body = document.querySelector('#content');
+body.style.backgroundColor = '';
